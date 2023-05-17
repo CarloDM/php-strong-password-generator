87a805e6ed0942d764fd94191cb49ef1d9a9bdb9
carloDM <carlo.de.mauro.work@gmail.com>
2023-05-17 17:26:59 +0200

    milstone 1 compl

--- index.php
+++ index.php
@@ -1,12 +1,44 @@
 <?php
 $passwordGenerated = '';
-$numb = 3;
-// $_GET['numb'];
+$numb = 0;
+
 if (array_key_exists('numb', $_GET)){
   $numb = $_GET['numb'];
 }else{
-  var_dump('nessun valore');
+  var_dump('nessun valore inserito per ora');
 }
+
+function genPass($numb){
+  $lettersMinString = 'a b c d e f g h i l m n o p q r s t u v z'; 
+  $lettersMinArray = explode(' ', $lettersMinString); /* 21 lettere*/
+  $lettersMiusc = 'A B C D E F G H I L M N O P Q R S T U V Z';
+  $lettersMaiuscArray = explode(' ', $lettersMiusc); /* 21 lettere*/
+  $simbols = '/ * - + ? ! " £ $ % & / ( ) =';
+  $simbolsArray = explode(' ', $simbols); /* 15 simboli*/
+  $passArray = [];
+
+  for ($i=0; $i < $numb ; $i++) { 
+    //selettore randomico
+    $rnd = rand(1,4); 
+    if($rnd === 1){
+      array_push($passArray,$simbolsArray[rand(0, 14)]);
+    }else if ($rnd === 2){
+      array_push($passArray,$lettersMaiuscArray[rand(0, 20)]);
+    }else if ($rnd === 3){
+      array_push($passArray,$lettersMinArray[rand(0, 20)]);
+    }else{
+      array_push($passArray,rand(0, 9));
+    }
+  }
+  
+  return implode('',$passArray);
+  // var_dump(implode('',$passArray));
+
+}
+
+
+
+
 
 ?>
 <!DOCTYPE html>
@@ -43,7 +75,7 @@ if (array_key_exists('numb', $_GET)){
   </form>
 
   <p class="mt-5">your generated password is:</p>
-  <p><?php echo $passwordGenerated . $numb ?></p>
+  <p><?php echo genPass($numb)?></p>
 
   </div>
   </div>
